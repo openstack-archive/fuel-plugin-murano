53745ecb4d7a597cccfabd7e534f480c82c0a74b
Denis Egorenko <degorenko@mirantis.com>
2016-03-03 17:51:15 +0300

    Initial commit for Murano plugin
    This commit adds:
    
    * deployment tasks;
    * UI settings;
    * plugin metadata;
    * deployment manifests;
    
    blueprint murano-fuel-plugin
    
    Change-Id: I077a6e16222bc33d0488af19e0789cc7c7308dfa

---
+++ deployment_scripts/manifests/murano_dashboard.pp
@@ -0,0 +1,30 @@
+notice('MURANO PLUGIN: murano_dashboard.pp')
+
+$murano_hash    = hiera_hash('murano', {})
+$murano_plugins = $murano_hash['plugins']
+$repository_url = has_key($murano_hash, 'murano_repo_url') ? {
+  true    => $murano_hash['murano_repo_url'],
+  default => 'http://storage.apps.openstack.org',
+}
+if has_key($murano_plugins, 'glance_artifacts_plugin') and $murano_plugins['glance_artifacts_plugin']['enabled'] {
+  $use_glare = true
+} else {
+  $use_glare = false
+}
+
+include ::murano::params
+include ::murano::client
+include ::horizon::params
+
+ensure_resource('service', 'httpd', {
+  'ensure'  => 'running',
+  'enable'  => true,
+  'restart' => true,
+  'name'    => $::horizon::params::http_service,
+})
+
+class { '::murano::dashboard':
+  enable_glare => $use_glare,
+  repo_url     => $repository_url,
+  sync_db      => false,
+}
