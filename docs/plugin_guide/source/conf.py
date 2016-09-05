0af357fcce30868d6d6ad7a81b2b87fad5b6cc12
Kirill Zaitsev <k.zaitsev@me.com>
2016-09-05 16:11:49 +0300

    Added release-notes to plugin guide
    Change-Id: I1b5fc3bf2df212d0fe70ee890a427ea317dc5aa6

--- docs/plugin_guide/source/conf.py
+++ docs/plugin_guide/source/conf.py
@@ -29,8 +29,8 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-#    'sphinx.ext.todo',
-#    'sphinx.ext.coverage',
+    'oslosphinx',
+    'reno.sphinxext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
