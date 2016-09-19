f2b65a07e8aae5883010984453fd9c80aea3031a
Denis Egorenko <degorenko@mirantis.com>
2016-09-19 19:29:38 +0300

    REMOVE OLD CORE LIBRARY
    WIP DNM
    
    Change-Id: Ic34d872441144ba5f1149a0d9c30f130edee4d1e

--- deployment_scripts/manifests/import_murano_package.pp
+++ deployment_scripts/manifests/import_murano_package.pp
@@ -1,4 +1,8 @@
 notice('MURANO PLUGIN: import_murano_package.pp')
 
+murano::application { 'io.murano-old' :
+  package_ensure => absent,
+  package_name   => 'io.murano',
+} ->
 murano::application { 'io.murano' : }
 murano::application { 'io.murano.applications' : }
