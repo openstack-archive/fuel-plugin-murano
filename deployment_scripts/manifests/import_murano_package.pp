68557690970f2e05fce0886d85c60a1ea0bddf5a
Denis Egorenko <degorenko@mirantis.com>
2016-09-19 19:29:38 +0300

    ALways upload latest package version
    Depends-on: I061cb64856e14d326dfa30851e427088e60f0a48
    Closes-bug: #1624723
    Change-Id: Ic34d872441144ba5f1149a0d9c30f130edee4d1e

--- deployment_scripts/manifests/import_murano_package.pp
+++ deployment_scripts/manifests/import_murano_package.pp
@@ -1,4 +1,8 @@
 notice('MURANO PLUGIN: import_murano_package.pp')
 
-murano::application { 'io.murano' : }
-murano::application { 'io.murano.applications' : }
+murano::application { 'io.murano' :
+  exists_action => 'u'
+}
+murano::application { 'io.murano.applications' :
+  exists_action => 'u'
+}
