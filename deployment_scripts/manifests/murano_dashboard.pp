4adad7a9844c687677dbe7197d3ec3a698b31a28
Denis Egorenko <degorenko@mirantis.com>
2016-11-01 16:14:42 +0300

    Restart glance-glare service after installing murano-glance plugin
    After installing package murano-glance-artifacts-plugin Glare service
    must be restarted, otherwise Murano will not work correctly with Glare.
    
    Change-Id: I773a1d778c33d66fed8a1f9cf6f5123ba90e9315
    Closes-bug: #1637508
    Related-bug: #1597368

--- deployment_scripts/manifests/murano_dashboard.pp
+++ deployment_scripts/manifests/murano_dashboard.pp
@@ -12,6 +12,11 @@ if has_key($murano_plugins, 'glance_artifacts_plugin') and $murano_plugins['glan
   package {'murano-glance-artifacts-plugin':
     ensure  => 'latest',
   }
+
+  include ::glance::params
+  ensure_resource('service', 'glance-glare',
+    { ensure => running, name => $::glance::params::glare_service_name })
+  Package['murano-glance-artifacts-plugin'] ~> Service['glance-glare']
 } else {
   $use_glare = false
 }
@@ -27,8 +32,6 @@ include ::horizon::params
 
 ensure_resource('service', 'httpd', {
   'ensure'  => 'running',
-  'enable'  => true,
-  'restart' => true,
   'name'    => $::horizon::params::http_service,
 })
 
