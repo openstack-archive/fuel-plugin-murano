93f6141107eccd47bade8826d19e9b09ca95c0ed
Denis Egorenko <degorenko@mirantis.com>
2016-11-10 15:20:38 +0300

    Fix disabling of old murano services
    Because of bug in fuel-yaql there is no other way as using
    some hardcoded ps-similar constructions.
    
    Change-Id: Ifcbe44c77f2266753b2916bcf2a06a9d37ee87d5

--- deployment_scripts/manifests/murano_disable_old.pp
+++ deployment_scripts/manifests/murano_disable_old.pp
@@ -1,25 +1,18 @@
 notice('MURANO PLUGIN: murano_disable_old.pp')
-include ::murano::params
 
-service { 'murano-rabbitmq':
-  ensure => 'stopped',
-}
-
-service { 'murano-api':
-  ensure => 'stopped',
-  name   => $::murano::params::api_service_name
-}
-
-service { 'murano-engine':
-  ensure => 'stopped',
-  name   => $::murano::params::engine_service_name
-}
-
-$base_cfapi   = hiera_hash('murano-cfapi', {})
-$plugin_cfapi = hiera_hash('murano_cfapi_plugin', {})
-if pick($base_cfapi['enabled'], false) or pick($plugin_cfapi['enabled'], false) {
-  service { 'murano-cfapi':
-    ensure => 'stopped',
-    name   => $::murano::params::cfapi_service_name
+define stop_service(
+  $service_name = $name,
+) {
+  exec { "stop ${name}":
+    command => "/usr/sbin/service ${service_name} stop || true",
+    onlyif => "/usr/sbin/service ${service_name} status",
   }
 }
+
+include ::murano::params
+$murano_services = [ $::murano::params::api_service_name ,
+                     $::murano::params::engine_service_name ,
+                     $::murano::params::cfapi_service_name ,
+                     'murano-rabbitmq' ]
+
+stop_service { $murano_services: }
